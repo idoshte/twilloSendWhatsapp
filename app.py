82b1cd92f9faf695d4a98f3664accc9317461a24
idoshte <idokole123@gmail.com>
2022-05-28 14:20:21 +0300

    add method urls

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, request, session
 import pandas as pd
 from twilio.twiml.messaging_response import MessagingResponse
 from fuzzywuzzy import fuzz,process
+from arcgis.gis import GIS
+import pandas as pd
+from fuzzywuzzy import fuzz,process
 
 
 # The session object makes use of a secret key.
@@ -13,7 +16,29 @@ app.config.from_object(__name__)
 def findSimalrties(val,mass):
    return fuzz.partial_ratio(mass,val)
 
+@app.route("/updateuUrl", methods=['GET', 'POST'])
+def url():
+    portal=GIS(username='kamanData',password='kamandata1234')
+    users_all = portal.users.search(query="username:kamanData") 
+    list_items = {}
+    def get_item(user):
+        content_item = user.items()
+        for item in content_item:
+            list_items[item.itemid] = item
+        folders = user.folders
+        for folder in folders :
+            folder_items = user.items(folder=folder['title'])
+            for item in folder_items:
+                list_items[item.itemid] = item
 
+    for i in range(len(users_all)):
+        print(f"extract {users_all[i]} data to excel...")
+        get_item(users_all[i])
+
+    df=pd.DataFrame(list_items)
+    df=df.transpose()
+    df.to_csv("allData.csv",index=False,encoding='utf-8')
+    return 'good request'
 
 @app.route("/sms", methods=['GET', 'POST'])
 def whatsapp():
